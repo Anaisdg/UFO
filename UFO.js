a3cf851ba374a6ae36ed36ec89abff8b28a06d36
Anais <dganais@gmail.com>
2018-02-10 09:58:13 -0600

    final changes

--- UFO.js
+++ UFO.js
@@ -1,15 +1,9 @@
-// Get references to the tbody element, input field and button
+
 var $tbody = document.querySelector("tbody");
 
-
-// Add an event listener to the searchButton, call handleSearchButtonClick when clicked
-// $searchBtn.addEventListener("click", handleSearchButtonClick);
-
-// Set filteredAddresses to addressData initially
 var filteredUFO = dataSet;
 
-// renderTable renders the filteredAddresses to the tbody
-function renderTable() {
+function renderTable(data) {
   $tbody.innerHTML = "";
   for (var i = 0; i < filteredUFO.length; i++) {
     // Get get the current address object and its fields
@@ -28,10 +22,19 @@ function renderTable() {
 
 renderTable();
 
+function queryParams() {
+    return {
+        type: 'owner',
+        sort: 'updated',
+        direction: 'desc',
+        per_page: 100,
+        page: 1
+    };
+}
+
 function filterTable(event) {
     var filter = event.target.value.toUpperCase();
     var rows = document.querySelector("#myTable tbody").rows;
-    console.log(rows)
 
     for (var i = 0; i < rows.length; i++) {
         var firstCol = rows[i].cells[0].textContent.toUpperCase();
@@ -41,26 +44,31 @@ function filterTable(event) {
         var sixthCol = rows[i].cells[5].textContent.toUpperCase();
         if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1 || fourthCol.indexOf(filter) > -1 || fifthCol.indexOf(filter) > -1 || sixthCol.indexOf(filter) > -1) {
             rows[i].style.display = "";
+            var newdataSet = rows[i].style.display;
         } else {
             rows[i].style.display = "none";
         }
     }
 }
+
+// filter bar for the city because only abbreviations were used
 
 function filterTable2(event) {
-    var filter = event.target.value.toUpperCase();
-    var rows = document.querySelector("#myTable tbody").rows;
-    console.log(rows)
+  var filter = event.target.value.toUpperCase();
+  var rows = document.querySelector("#myTable tbody").rows;
+  console.log(rows)
 
-    for (var i = 0; i < rows.length; i++) {
-        var thirdCol = rows[i].cells[2].textContent.toUpperCase();
-        if (thirdCol.indexOf(filter) > -1) {
-            rows[i].style.display = "";
-        } else {
-            rows[i].style.display = "none";
-        }
+  for (var i = 0; i < rows.length; i++) {
+    var thirdCol = rows[i].cells[2].textContent.toUpperCase();
+    if (thirdCol.indexOf(filter) > -1) {
+      rows[i].style.display = "";
+    } else {
+      rows[i].style.display = "none";
     }
+  }
+
 }
+
 
 document.querySelector('#myInput').addEventListener('keyup', filterTable, false);
 document.querySelector('#myInput2').addEventListener('keyup', filterTable2, false);
